8c8c00d673d098dab6143070bddc08a606c34461
Moeez Ahmed <107947151+MoeezKing@users.noreply.github.com>
2023-03-31 19:30:04 +0500

    Create smallestNumber.dart

---
+++ smallestNumber.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+void main()
+{
+  stdout.write('Enter 1st number :-');
+  double? n1=double.parse(stdin.readLineSync()!);
+  stdout.write('Enter 2nd number :-');
+  double? n2=double.parse(stdin.readLineSync()!);
+  stdout.write('Enter 3rd number :-');
+  double? n3=double.parse(stdin.readLineSync()!);
+  print('Smallest number among then is ${smallestNum(n1, n2, n3)}');
+}
+double smallestNum(double a,double b,double c)
+{
+  if(a<b)
+    {
+      if(a<c)
+        return a;
+      else
+        return c;
+    }else
+      {
+        if(b<c)
+          return b;
+        else
+          return c;
+      }
+}
