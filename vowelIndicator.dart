4bc2db5dc820f6e91452bc7c055d3c4e66cbb27a
Moeez Ahmed <107947151+MoeezKing@users.noreply.github.com>
2023-03-31 17:57:03 +0500

    Create vowelIndicator.dart

---
+++ vowelIndicator.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+
+void main()
+{
+  String chr=inputChr();
+  if(isVowel(chr))
+    {
+      print('The entered character is vowel.');
+    }
+  else
+    {
+      print('The entered character is not vowel.');
+    }
+  
+}
+String inputChr()
+{
+  stdout.write('Enter a character to know whether it is Vowel or not =->');
+  String? str=stdin.readLineSync();
+
+  if (str==null?true:str.length != 1) {
+    print('Invalid output . You have to Enter a single Character');
+    str = inputChr();
+  }
+
+  return str;
+}
+bool isVowel(String c)
+{
+  if(c=='A'||c=='a')
+    {
+      return true;
+    }else if(c=='E'||c=='e')
+    {
+      return true;
+    }else if(c=='I'||c=='i')
+  {
+    return true;
+  }else if(c=='O'||c=='u')
+  {
+    return true;
+  }else if(c=='U'||c=='u')
+  {
+    return true;
+  }else{
+    return false;
+  }
+
+}
