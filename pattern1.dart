70d8a7d1aaf69a18e547995812d4c904f6058d1d
Moeez Ahmed <107947151+MoeezKing@users.noreply.github.com>
2023-04-01 17:06:52 +0500

    Create pattern1.dart

---
+++ pattern1.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+void main()
+{
+  for(int i=5;i>=1;i--)
+    {
+      for(int j=1;j<=i;j++)
+        stdout.write('@');
+      print('\n');
+    }
+}
+
+/*
+@@@@@
+
+@@@@
+
+@@@
+
+@@
+
+@
+*/
